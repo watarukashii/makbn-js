9b4aea33df0216bde7c4f2ba5b37715b0efac329
watarukashii <kashii@itm.ne.jp>
2013-11-23 15:28:57 +0900

    画像をランダムに表示、画像待機時間を動的化

--- js/jquery.mkbnSlider-0.0.1.js
+++ js/jquery.mkbnSlider-0.0.1.js
@@ -38,24 +38,41 @@ $.fn.mkbnSlider = function(options) {
 
 	// 再帰
 	function slide () {
-		// currentIndexに1を加算する
-		// currentIndex = currentIndex +1;　と同じ意味
-		currentIndex += 1;
-		// 現在のliの番号がいliの数を超えた時一番最後の画像にきたら、幅と番号を初期化
-		if (currentIndex > liLength - 1) {
-			currentIndex = 0;
-		}
-		// setTimeout(処理、何ミリ秒後)
-		setTimeout(function(){
+			// currentIndexに1を加算する
+			// currentIndex = currentIndex +1;　と同じ意味
+			currentIndex += 1;
+			// 現在のliの番号がいliの数を超えた時一番最後の画像にきたら、幅と番号を初期化
+			if (currentIndex > liLength - 1) {
+				currentIndex = 0;
+				if (options.IsRepeat === false) {
+					return;
+				}
+			}
+			// setTimeout(処理、何ミリ秒後)
+			setTimeout(function(){
+
+			//durationを決定する
+			var duration = options.duration;
+			var durationRandomRangeMin = 500;
+			var durationRandomRangeMax = 10000;
+
+			// duration の定義 ランダム表示
+			if (options.isDurationRandom) {
+				duration = Math.random() * (durationRandomRangeMax - durationRandomRangeMin) + durationRandomRangeMin;
+			}
+
 			$ul
-			.animate({ left: liWidth * -1 * currentIndex })
+			.animate(
+				{ left: liWidth * -1 * currentIndex },
+				{ duration: duration }	// {名前:変数}
+			)
 			// animate実行した後にそのアニメーションが実行してるか監視
 			.promise()
 			// animateがちゃんと実行された後に実行したい処理を入れる。
 			// done(slide())だと、アニメーションが実行する前にslideが実行されてしまうため、
 			// 実行したい関数を変数として明示的に指定しておく
 			.done(slide);
-		}, 600);
+		}, options.wait);
 	}
 	
 	// 再帰関数を実行
